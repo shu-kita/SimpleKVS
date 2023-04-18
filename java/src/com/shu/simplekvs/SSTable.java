2050e5539ca2375e628bff25acdeac9c9d8fbc70
shu-kita <shusei3316@yahoo.co.jp>
2023-04-18 23:54:17 +0900

    add SSTable constracter

--- java/src/com/shu/simplekvs/SSTable.java
+++ java/src/com/shu/simplekvs/SSTable.java
@@ -1,6 +1,10 @@
 package com.shu.simplekvs;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+
 import java.util.Map;
 
 public class SSTable {
@@ -12,4 +16,32 @@ public class SSTable {
     // getメソッド
     // deleteメソッド(ファイルの消去メソッド)
     // (その他、Pythonの時必要だったメソッドを見ながら実装する方針)
+
+    public SSTable(String path, Map<String, String> memtable) {
+        this.path = Paths.get(path);
+
+        Long timestamp = System.currentTimeMillis();
+
+        if (Files.isDirectory(this.path)) {
+            Path file = Paths.get(String.format("sstab_%d.dat", timestamp));
+            this.path = this.path.resolve(file);
+        }
+
+        //memtable書き込み処理
+    }
+
+    public SSTable(String path) throws FileNotFoundException{
+        this.path = Paths.get(path);
+        
+        if (!Files.exists(this.path)) {
+            throw new FileNotFoundException(String.format("%s is not found.", path));
+        }
+
+        // Index読み込み処理。
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        // System.out.println("test");
+        SSTable s = new SSTable("./adfsaf");
+    }
 }
